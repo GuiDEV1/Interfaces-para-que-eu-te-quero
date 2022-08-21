901b104980c642a6fe36006759fdef187e4e8d31
Guilherme <100242616+GuiDEV1@users.noreply.github.com>
2022-08-21 15:15:48 -0300

    Add files via upload

---
+++ pesquisa_interface_para_que_te_quero/script.js
@@ -0,0 +1,7 @@
+function acionaCor() {
+    document.getElementById('logo').style.background = '#000';
+}
+
+function acionaCoro() {
+    document.getElementById('logo').style.background = '#e60c0c'; 
+}
